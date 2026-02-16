ae247ed68a7aee8367d9b17d9c6297162b015638
tharuka <trdilshan99@gmail.com>
2026-02-16 16:08:21 +0000

    a

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command, mode }) => {
   return {
     plugins: [react()],
+    base: mode === 'production' ? '/BitryxWebSite/' : '/',
   };
 })
